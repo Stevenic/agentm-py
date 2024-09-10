53f1e95cf3198b1053d48ed7943d22b5a096d0eb
Jochen Schultz <jochenschultz@gmx.de>
2024-09-10 21:32:23 +0000

    added core functionality and tests for that, added an install.py file which creates a db for token handling, documentation and updated ci.yml

---
+++ src/core/concurrency.py
@@ -0,0 +1,15 @@
+import asyncio
+
+class Semaphore:
+    def __init__(self, max_concurrent_tasks):
+        self.semaphore = asyncio.Semaphore(max_concurrent_tasks)
+
+    async def __aenter__(self):
+        await self.semaphore.acquire()
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        self.semaphore.release()
+
+    async def call_function(self, func, *args, **kwargs):
+        async with self.semaphore:
+            return await func(*args, **kwargs)
---
+++ src/core/parallel_complete_prompt.py
@@ -0,0 +1,15 @@
+import asyncio
+from .concurrency import Semaphore
+
+class ParallelCompletePrompt:
+    def __init__(self, complete_prompt_func, parallel_completions=1, should_continue_func=None):
+        self.complete_prompt_func = complete_prompt_func
+        self.parallel_completions = parallel_completions
+        self.should_continue_func = should_continue_func or (lambda: True)
+        self.semaphore = Semaphore(parallel_completions)
+
+    async def complete_prompt(self, *args, **kwargs):
+        async with self.semaphore:
+            if not self.should_continue_func():
+                raise asyncio.CancelledError("Operation cancelled.")
+            return await self.complete_prompt_func(*args, **kwargs)
